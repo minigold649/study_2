ce423f3fb4fb6da52b5367ecb1946ebc381e2343
akzmwhgdk <akzmwhgdk@DESKTOP-FDRV90N.mshome.net>
2020-10-08 20:56:58 +0900

    a

---
+++ src/a/a.java
@@ -0,0 +1,20 @@
+package a;
+
+import java.util.Scanner;
+
+public class a {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		String input = s.next();
+		char palindrom[] = new char[input.length()];
+		
+		for (int i = 0 ; i < (input.length()) ; i++) {
+			
+		}
+		
+		s.close();
+	}
+
+}
